5d4b4380b24e8c44927ac869822eef5e800b659f
Cereopsis <Cereopsis@users.noreply.github.com>
2016-10-20 14:47:42 +0100

    Make public to allow importing as a package

--- Coprime.swift
+++ Coprime.swift
@@ -22,30 +22,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-struct Coprime {
+public struct Coprime {
     
-    let m: Int
-    let n: Int
+    public let m: Int
+    public let n: Int
     
-    init(m: Int, n: Int) {
+    public init(m: Int, n: Int) {
         self.m = m
         self.n = n
     }
     
     /// Returns product of `m` * `n`
-    func product() -> Int {
+    public func product() -> Int {
         return m * n
     }
     
     /// Returns true iff m == 1 || n == 1
-    var isIdentity: Bool {
+    public var isIdentity: Bool {
         return m == 1 || n == 1
     }
     
     /// Returns branch 1 (See https://en.wikipedia.org/wiki/Coprime_integers)
     ///
     /// This branch increases less rapidly than 2 and less montonically than 3
-    var b0: Coprime {
+    public var b0: Coprime {
         return Coprime(m: 2 * m - n, n: m)
         
     }
@@ -53,18 +53,18 @@ struct Coprime {
     /// Returns branch 2
     ///
     /// This branch increases rapidly
-    var b1: Coprime {
+    public var b1: Coprime {
         return Coprime(m: 2 * m + n, n: m)
     }
     
     /// Branch 3
     ///
     /// This branch increases typically by (m + 2 or 3, n), so rather monotonically
-    var b2: Coprime {
+    public var b2: Coprime {
         return Coprime(m: m + 2 * n, n: n)
     }
     
-    func filter(_ predicate: (Coprime) -> Bool) -> [Coprime] {
+    public func filter(_ predicate: (Coprime) -> Bool) -> [Coprime] {
         return [b0, b1, b2].filter(predicate)
     }
 }
